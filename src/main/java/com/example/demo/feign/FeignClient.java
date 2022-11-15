135d937f8cda7fd15489d08ae05dfc02a7732255
nikod <nikodim.curo4kin@yandex.ru>
2022-11-15 18:15:38 +0300

    Feign interface.

--- src/main/java/com/example/demo/feign/FeignClient.java
+++ src/main/java/com/example/demo/feign/FeignClient.java
@@ -1,7 +1,5 @@
 package com.example.demo.feign;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
 @org.springframework.cloud.openfeign.FeignClient(value = "FeignClient", url = "")
 public interface FeignClient {
 }
