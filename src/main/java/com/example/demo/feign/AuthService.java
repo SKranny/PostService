0c7e4e3dd884f99f914da7d43e5cf69cacc7ba53
Дмитрий Кечин <kechin.d@gmail.com>
2023-01-16 18:27:39 +0300

    refactor ci/cd

--- src/main/java/com/example/demo/feign/AuthService.java
+++ src/main/java/com/example/demo/feign/AuthService.java
@@ -1,9 +1,7 @@
 package com.example.demo.feign;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 @FeignClient("auth-service/api/v1/auth")
 public interface AuthService {
