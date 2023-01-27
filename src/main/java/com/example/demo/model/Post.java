35e7e6ea29a713ec1888f3589fd6646bb6c45255
Oleg Shmelev <o.shmelev76@gmail.com>
2023-01-27 13:59:10 +0300

    fix remarks + fix conflict

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -2,7 +2,7 @@ package com.example.demo.model;
 
 import lombok.*;
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
