8d078b8104098c9502c5e969b4c3495f8a126b96
Oleg Shmelev <o.shmelev76@gmail.com>
2023-02-18 10:53:09 +0300

    add Graph entity

--- src/main/java/com/example/demo/repositories/TagRepository.java
+++ src/main/java/com/example/demo/repositories/TagRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repositories;
 
 import com.example.demo.model.Tag;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,7 @@ import java.util.Optional;
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Optional<Tag> findByTagIgnoreCase(String text);
+
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = "tag")
     List <Tag> findAllByTagIn(Collection<String> tags);
 }
