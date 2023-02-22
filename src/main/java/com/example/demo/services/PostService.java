33da8aaa9f088b62657336e45ded912b679d979d
nikod <nikodim.curo4kin@yandex.ru>
2023-02-22 15:36:52 +0300

    createNotif has been added.

--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -99,6 +99,7 @@ public class PostService {
                 .publishTime(publishTime)
                 .tags(getOrBuildTags(req.getTags()))
                 .build();
+        createNotification(post);
         postRepository.save(post);
     }
 
