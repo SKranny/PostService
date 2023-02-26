0e33263d917906d93cad47f62158219e11f565e7
Дмитрий Кечин <kechin.d@gmail.com>
2023-02-26 23:32:00 +0300

    add Security

--- src/main/java/com/example/demo/controllers/CommentController.java
+++ src/main/java/com/example/demo/controllers/CommentController.java
@@ -51,6 +51,7 @@ public class CommentController {
     public List<CommentDTO> getAllCommentsByText(@PathVariable Long postId, @PathVariable String text) {
         return commentService.getAllCommentsByText(postId, text);
     }
+
     @Operation(summary = "Создать комментарий")
     @PostMapping("/{id}/comment")
     @ResponseBody
